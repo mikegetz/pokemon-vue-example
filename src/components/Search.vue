57020c5dfd7a03a1a6ec433491805ab2c561d0fd
mikegetz <getzmd02@gmail.com>
2023-09-09 20:06:18 -0700

    restyled search component

--- src/components/Search.vue
+++ src/components/Search.vue
@@ -18,11 +18,11 @@ const makeRequest = async () => {
 
 <template>
     <div class="max-w-screen-lg m-auto mt-24">
-    <div class="grid grid-cols-6 gap-4">
-      <div class="col-start-2 col-span-3">
+    <div class="grid grid-cols-8 gap-4">
+      <div class="col-start-3 col-span-3">
         <input type="text" v-model="searchInput" v-on:keyup.enter="makeRequest" placeholder="search pokemon"/>
       </div>
-      <button @click="makeRequest" class="btn btn-blue">Search</button>
+        <button @click="makeRequest" class="btn btn-blue">Search</button>
     </div>
     <div class="grid grid-cols-4 gap-2">
       <img v-bind:src="pokemonImg"/>
