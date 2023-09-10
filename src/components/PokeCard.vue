801925051738df6a369e5e34269d1055b98eb7fa
mikegetz <getzmd02@gmail.com>
2023-09-09 21:34:18 -0700

    removing HelloWorld from template, adding pokecard component with styling, adding name and description to search

---
+++ src/components/PokeCard.vue
@@ -0,0 +1,37 @@
+<script setup lang="ts">
+
+defineProps({
+  pokemonImg: String,
+  pokemonName: String,
+  pokemonDescription: String,
+})
+
+</script>
+
+<template>
+  <div class="card">
+    <div class="grid grid-cols-8">
+      <div class="col-start-2 col-span-6">
+        <img v-bind:src="pokemonImg"/>
+      </div>
+    </div>
+    <div class="grid grid-cols-4 justify-items-center title">
+      <div class="col-start-2 col-span-2">
+        {{ pokemonName }}
+      </div>
+      <div class="col-span-4">
+        {{ pokemonDescription }}
+      </div>
+    </div>
+  </div>
+</template>
+
+<style scoped>
+.card {
+  box-shadow: 0 3px 10px rgb(0 0 0 / 0.4);
+}
+.title {
+  margin-bottom: 10px;
+  margin-top: 4px;
+}
+</style>
--- src/components/Search.vue
+++ src/components/Search.vue
@@ -1,31 +1,48 @@
 <script setup lang="ts">
 import { ref } from 'vue';
-import { pokemonRequest } from '../api/pokemon-request';
+import { pokemonRequest, genericPokemonAPIRequest } from '../api/pokemon-request';
+import PokeCard from './PokeCard.vue';
 
 const searchInput = ref('');
 const pokemonImg = ref('');
+const pokemonName = ref('');
+const pokemeonDescription = ref('');
 
 const makeRequest = async () => {
   try {
-    const result = await pokemonRequest(searchInput.value.toLowerCase());
-    pokemonImg.value = result.data.sprites.other["official-artwork"].front_default
+    const pokemonResult = await pokemonRequest(searchInput.value.toLowerCase());
+    pokemonImg.value = pokemonResult.data.sprites.other["official-artwork"].front_default;
+    pokemonName.value = pokemonResult.data.name;
+    pokemeonDescription.value = await loadDescription(pokemonResult.data.species.url);
   } catch (e) {
     //TODO: banner
   }
   searchInput.value = "";
 };
+
+const loadDescription = async (url) => {
+  const speciesResult = await genericPokemonAPIRequest(url);
+  let descriptions = speciesResult.data.flavor_text_entries;
+  descriptions = descriptions.filter((description) => description.language.name === "en");
+  descriptions = descriptions.slice(0, 5);
+  descriptions = descriptions.map((description) => description.flavor_text);
+  descriptions = descriptions.filter((description, index) => descriptions.indexOf(description) === index);
+  descriptions = descriptions.map((description) => description.replace(/[^a-z0-9]/gim, " ").trim());
+  const description = descriptions.join(". ").concat(".");
+  return description;
+};
 </script>
 
 <template>
-    <div class="max-w-screen-lg m-auto mt-24">
+    <div class="ml-20 mt-24">
     <div class="grid grid-cols-8 gap-4">
       <div class="col-start-3 col-span-3">
         <input type="text" v-model="searchInput" v-on:keyup.enter="makeRequest" placeholder="search pokemon"/>
       </div>
         <button @click="makeRequest" class="btn btn-blue">Search</button>
     </div>
-    <div class="grid grid-cols-4 gap-2">
-      <img v-bind:src="pokemonImg"/>
+    <div v-if="pokemonImg" class="grid grid-cols-4 gap-2">
+      <PokeCard :pokemonImg="pokemonImg" :pokemonName="pokemonName" :pokemonDescription="pokemeonDescription"/>
     </div>
   </div>
 </template>
