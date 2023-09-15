2e7203bd29d72d4292acc694426445698a6e2820
mikegetz <getzmd02@gmail.com>
2023-09-14 19:58:16 -0700

    restyled, added horizontal rule

--- src/components/PokeCard.vue
+++ src/components/PokeCard.vue
@@ -11,15 +11,17 @@ defineProps({
 <template>
   <div class="card">
     <div class="grid grid-cols-8">
-      <div class="col-start-2 col-span-6">
+      <div class="col-start-2 col-span-6 card-image">
         <img v-bind:src="pokemonImg"/>
       </div>
     </div>
-    <div class="grid grid-cols-4 justify-items-center title">
-      <div class="col-start-2 col-span-2">
+    <div class="grid grid-cols-4 justify-items-center">
+      <div class="col-start-2 col-span-2 title">
         {{ pokemonName }}
       </div>
-      <div class="col-span-4">
+      <div class="col-span-4 border-t horizontal-rule">
+      </div>
+      <div class="col-span-4 description">
         {{ pokemonDescription }}
       </div>
     </div>
@@ -31,7 +33,19 @@ defineProps({
   box-shadow: 0 3px 10px rgb(0 0 0 / 0.4);
 }
 .title {
-  margin-bottom: 10px;
-  margin-top: 4px;
+  padding-bottom: 4px;
+  padding-top: 4px;
+}
+.description {
+  padding: 5px 12px 12px 12px;
+  font-size: 14px;
+}
+.horizontal-rule {
+  border-color:black;
+  padding-bottom: 5px;
+  width: 100%;
+}
+.card-image {
+  padding-top: 5px;
 }
 </style>
