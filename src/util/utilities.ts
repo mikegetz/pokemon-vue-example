4e654ecb9ad9a9761985440f840439a9d1efc4dd
mikegetz <getzmd02@gmail.com>
2023-09-16 20:12:49 -0700

    trimming once

--- src/util/utilities.ts
+++ src/util/utilities.ts
@@ -5,8 +5,9 @@ export const dedup = <T>(inputArray: Array<T>): Array<T> => {
 
 export const cleanDescriptionsIntoDescription = (descriptions: Array<string>): string => {
   let parsedDescriptions = descriptions.map((description) => description.replace(/POK.MON/g, "Pokemon"));
-  parsedDescriptions = parsedDescriptions.map((description) => description.replace(/\u000c/u, " ").trim());
-  parsedDescriptions = parsedDescriptions.map((description) => description.replace(/\n/g, " ").trim());
+  parsedDescriptions = parsedDescriptions.map((description) => description.replace(/\u000c/u, " "));
+  parsedDescriptions = parsedDescriptions.map((description) => description.replace(/\n/g, " "));
+  parsedDescriptions = parsedDescriptions.map((description) => description.trim());
   const description = parsedDescriptions.join(" ");
   return description;
 }
